ba8ef75557c68ac120cd012e7870c1e2ec941794
Trevor Klinkenberg <tkndf@mail.umkc.edu>
2017-10-03 09:10:37 -0500

    Still learning this push thing.

---
+++ Project1BTeam15/Project1BTeam15.cpp
@@ -0,0 +1,41 @@
+// Project1BTeam15.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "hello world";
+	//TODO: These Functions are what need to be called and implemented in the main:
+	/*
+	// Need Library Class here
+	Library library;
+	// these will be variables in the library class
+	library.add_book("Software Engineering");
+	library.add_book("Chemistry");
+	library.add_Employee("Adam");
+	library.add_employee("Sam");
+	library.add_employee("Ann");
+
+	// Function Call to Library class that should be sent on externally to a logic class
+	library.circulate_book("Chemistry", Date(2015, 3, 1, DateFormat::US));
+	library.circulate_book("Software Engineering", Date(2015, 3, 5, DateFormat::US));
+
+	//Pass on books
+	library.pass_on("Chemistry", Date(2015, 3, 7, DateFormat::US));
+	// Book should be archived with this call:
+	library.pass_on("Chemistry", Date(2015, 3, 15, DateFormat::US));
+
+	// Software Engineering pass
+	library.pass_on("Software Engineering", Date(2015, 4, 5, DateFormat::US));
+	library.pass_on("Software Engineering", Date(2015, 4, 10, DateFormat::US));
+	library.pass_on("Software Engineering", Date(2015, 4, 15, DateFormat::US));
+	
+	*/
+
+
+    return 0;
+}
+
